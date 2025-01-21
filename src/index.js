4d56151f4c03dbb8a72e3c5552db0ca63e64406d
atheer-a93 <113246927+atheer-a93@users.noreply.github.com>
2025-01-21 16:39:06 +0300

    Updated index.html, package.json, and src/index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
-import { BrowserRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router basename="/Educate">
-    <App />
-  </Router>
-);
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Create the root element where React will mount the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the React app into the root element
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
->>>>>>> 2588bcfee64bb7a7ce03c91d6453578fd8a750d5
